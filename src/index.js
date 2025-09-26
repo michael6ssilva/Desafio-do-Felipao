c8a08fa82d1762e1f72b253fbe856f0a967132b7
michael6ssilva <michael6ssilva@gmail.com>
2025-09-26 12:59:37 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// Importa biblioteca para ler entradas do usuário
+const prompt = require("prompt-sync")();
+
+// Entrada de dados
+const nome = prompt("Digite o nome do herói: ");
+const xp = parseInt(prompt("Digite a quantidade de XP: "));
+
+let nivel = "";
+
+// Estrutura de decisão para classificar o herói
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+}
+
+// Saída final
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
